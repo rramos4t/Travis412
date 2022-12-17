526ef74ba6df86e996fab52d3104bf8d98921b71
rramos4t <116299957+rramos4t@users.noreply.github.com>
2022-12-17 03:00:28 -0500

    test error

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,8 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.minus(5, 3);
+	}
 }
